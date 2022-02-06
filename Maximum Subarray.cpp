ebcc9ea6ff4c9f4715fc5817b25c7e91fdcc98e6
Aditya Gattu <52188412+AdityaGattu@users.noreply.github.com>
2022-02-06 11:36:36 +0530

    Problem 4

---
+++ Maximum Subarray.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/maximum-subarray/
+
+//Problem-4
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum = 0 ;
+        int max_ele = nums[0];
+        
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            max_ele = max(max_ele,sum);       //if currrsum > max_sum we update
+            if(sum<0){              //if currsum<0 no need add or carry forword
+                sum=0;
+            }
+        }
+        
+        return max_ele;
+        
+    }
+};

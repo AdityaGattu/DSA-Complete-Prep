15820b92b98b317a92234f5046cff07c9b88fe9d
Aditya Gattu <52188412+AdityaGattu@users.noreply.github.com>
2022-02-20 20:59:34 +0530

    Problem 11

--- Q11. Find the repeating and missing numbers.cpp
+++ Q11. Find the repeating and missing numbers.cpp
@@ -1,4 +1,6 @@
+https://www.interviewbit.com/problems/repeat-and-missing-number-array/
 
+Sol1 : Using simple Maths:
 
 vector<int> Solution::repeatedNumber(const vector<int> &A) {
    vector<int>ans;
@@ -25,3 +27,63 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
     
     return ans;
 }
+
+
+Sol2 : Diving into 2 sets: 
+
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    
+    vector<int>ans;
+    int S=0;
+
+    for(int i=1;i<=A.size();i++){
+        S ^= i^A[i-1];       //s =X^Y   X-missing_no minus Y-repeated_no
+    }
+
+    //find the right most max bit
+    int max_bit_no = S & ~(S-1);  //max_bit_no nothing but set_bit_no;
+
+    int x=0;
+    int y=0;
+
+    for(int i=0;i<A.size();i++){
+        if(A[i]&max_bit_no){
+            x^=A[i];
+        }
+        else{
+            y^=A[i];
+        }
+        if((i+1)&max_bit_no){
+            x^=i+1;
+        }
+        else{
+            y^=i+1;
+        }
+    } 
+
+    int x_cnt=0;
+    for(int i=0;i<A.size();i++){
+        if(x==A[i])
+        {
+            x_cnt++;
+        }
+    }
+    int missing_no;
+    int repeated_no;
+
+    if(x_cnt==0){
+        missing_no=x;
+        repeated_no=y;
+    }
+    else{
+        missing_no=y;
+        repeated_no=x;
+    }
+
+
+    ans.push_back(repeated_no);
+    ans.push_back(missing_no);
+    
+    return ans;
+}
+
